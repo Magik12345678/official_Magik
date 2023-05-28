a0909b10b399f5411474d8f61d859920ecbc769f
Magik12345678 <134856514+Magik12345678@users.noreply.github.com>
2023-05-28 17:40:50 +0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="icon" type="image" href="https://w7.pngwing.com/pngs/251/870/png-transparent-silver-haired-female-anime-character-anime-puzzles-japanese-anime-jigsaw-puzzles-anime-music-video-computer-icons-anime-logo-game-manga-computer-wallpaper.png">
+    <title> Топы аниме</title>
+    <link rel="stylesheet" href="style.css">
+    </head>
+  <body class = "bodystart">
+<main class = start>
+    <div class = "card1"><h2><a href = "1.html" target="_blank" >Топы аниме фентези</a></h2>
+    <img src="https://kartinkin.net/uploads/posts/2021-07/1626719235_7-kartinkin-com-p-anime-fentezi-romantika-shkola-anime-krasi-7.jpg"alt="картинка на обложке" width="400"></div>
+    <div class = "card2"><h2><a href = "2.html" target="_blank" >Топы аниме иссекаи</a></h2>
+    <img src="https://avatars.dzeninfra.ru/get-zen_doc/1881575/pub_603d998bbdd71022a239c3eb_603d9c18a3c2fb47749801de/scale_1200" alt="картинка на обложке" width="400"></div>
+   <div class = "card3"><h2><a href = "3.html" target="_blank" >Топы аниме романтика</a></h2>
+    <img src="https://i.ytimg.com/vi/yJJZJvxa10A/maxresdefault.jpg" alt="картинка на обложке" width="400"></div>
+    </main>
+  </body>
+</html>
